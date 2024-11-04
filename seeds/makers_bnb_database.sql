05ad73794840ab6ad18d6b4130487e79c96ddc50
Abubakar Shariffali <110543733+bakarhs@users.noreply.github.com>
2024-11-04 16:48:16 +0000

    Update makers_bnb_database.sql
    .

--- seeds/makers_bnb_database.sql
+++ seeds/makers_bnb_database.sql
@@ -10,11 +10,11 @@ CREATE TABLE people (
     user_password VARCHAR(255)
 );
 
-INSERT INTO properties (id, first_name, surname, username, user_password) VALUES ('', 'Khalid', 'Ham', 'KHam', 'Python24');
-INSERT INTO properties (id, first_name, surname, username, user_password) VALUES ('', 'Bakar', 'Shariffali', 'BShariffali', 'CSharp60');
-INSERT INTO properties (id, first_name, surname, username, user_password) VALUES ('', 'Lou', 'Paine', 'LPaine', 'Fortran90');
-INSERT INTO properties (id, first_name, surname, username, user_password) VALUES ('', 'Alberto', 'Tobarra', 'ATobarra', 'Java54');
-INSERT INTO properties (id, first_name, surname, username, user_password) VALUES ('', 'John', 'O Neill', 'JONeill', 'HTML30');
+INSERT INTO properties (first_name, surname, username, user_password) VALUES ('Khalid', 'Ham', 'KHam', 'Python24');
+INSERT INTO properties (first_name, surname, username, user_password) VALUES ('Bakar', 'Shariffali', 'BShariffali', 'CSharp60');
+INSERT INTO properties (first_name, surname, username, user_password) VALUES ('Lou', 'Paine', 'LPaine', 'Fortran90');
+INSERT INTO properties (first_name, surname, username, user_password) VALUES ('Alberto', 'Tobarra', 'ATobarra', 'Java54');
+INSERT INTO properties (first_name, surname, username, user_password) VALUES ('John', 'O Neill', 'JONeill', 'HTML30');
 
 
 CREATE TABLE properties (
@@ -28,11 +28,11 @@ CREATE TABLE properties (
     FOREIGN KEY (person_owner_id) REFERENCES people(id)
 );
 
-INSERT INTO properties (id, property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('', 'The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1);
-INSERT INTO properties (id, property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('', 'The Laurels', '56 Secret Bay', 'Reykjavik', 'Cold', 30, 2);
-INSERT INTO properties (id, property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('', 'The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3);
-INSERT INTO properties (id, property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('', 'The Bananas', '1730 Clark St', 'Chicago', 'Windy', 65, 4);
-INSERT INTO properties (id, property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('', 'The Pines', '435 Melbourne Ave', 'Adelaide', 'Very Warm', 110, 5);
+INSERT INTO properties (property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1);
+INSERT INTO properties (property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('The Laurels', '56 Secret Bay', 'Reykjavik', 'Cold', 30, 2);
+INSERT INTO properties (property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3);
+INSERT INTO properties (property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('The Bananas', '1730 Clark St', 'Chicago', 'Windy', 65, 4);
+INSERT INTO properties (property_name, street_address, city, property_description, price_per_night, person_owner_id) VALUES ('The Pines', '435 Melbourne Ave', 'Adelaide', 'Very Warm', 110, 5);
 
 
 CREATE TABLE bookings (

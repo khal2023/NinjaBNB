9b658184b25c6d15e77f5c4098c6d817be337949
Abubakar Shariffali <110543733+bakarhs@users.noreply.github.com>
2024-11-05 11:37:57 +0000

    Update and rename test_person_repo.py to test_users_repo.py
    Changed name conventions. person --> users

--- tests/test_users_repo.py
+++ tests/test_users_repo.py
@@ -1,32 +1,32 @@
-from lib.person_repo import PersonRepo
-from lib.person import Person
+from lib.users_repo import UsersRepo
+from lib.users import Users
 
 
-# find person from db based on person_id
-def test_find_person(db_connection):
+# find users from db based on users_id
+def test_find_users(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
-    repo = PersonRepo(db_connection)
-    assert repo.find(1) == Person(1, 'Khalid', 'Ham', 'KHam', None)
+    repo = UsersRepo(db_connection)
+    assert repo.find(1) == Users(1, 'Khalid', 'Ham', 'KHam', None)
 
 def test_find_username_in_db(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
-    repo = PersonRepo(db_connection)
+    repo = UsersRepo(db_connection)
     assert repo.username_in_db("KHam") == True
     assert repo.username_in_db("MarkAnthony") == False
 
-# # create person in db if they pass username and password validation tests
-def test_create_person(db_connection):
+# # create users in db if they pass username and password validation tests
+def test_create_users(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
-    repo = PersonRepo(db_connection)
+    repo = UsersRepo(db_connection)
     repo.create("Hunor", "Tamas", "C0deMaster", "Apple24!")
     assert repo.username_in_db("C0deMaster") == True
 
 def test_create_refuses_incorrect_password(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
-    repo = PersonRepo(db_connection)
+    repo = UsersRepo(db_connection)
     repo.create("Hunor", "Tamas", "C0deMaster", "Apple!")
     assert repo.username_in_db("C0deMaster") == False
 
 
 
-# remove person from db based on selection criteria 
+# remove users from db based on selection criteria 

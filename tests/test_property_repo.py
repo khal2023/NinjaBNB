7ea38c890442daee88461afc88cb2f37473923ba
Abubakar_Shariffali <bakarhs123@gmail.com>
2024-11-05 15:53:26 +0000

    TDD property_repo

--- tests/test_property_repo.py
+++ tests/test_property_repo.py
@@ -8,41 +8,41 @@ def test_get_all_properties(db_connection):
     properties = repo.all() 
 
     assert properties == [
-        Property('The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1),
-        Property('The Laurels', '56 Secret Bay', 'Reykjavik', 'Cold', 30, 2),
-        Property('The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3),
-        Property('The Bananas', '1730 Clark St', 'Chicago', 'Windy', 65, 4),
-        Property('The Pines', '435 Melbourne Ave', 'Adelaide', 'Very Warm', 110, 5),
+        Property(1,'The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1),
+        Property(2,'The Laurels', '56 Secret Bay', 'Reykjavik', 'Cold', 30, 2),
+        Property(3,'The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3),
+        Property(4,'The Bananas', '1730 Clark St', 'Chicago', 'Windy', 65, 4),
+        Property(5,'The Pines', '435 Melbourne Ave', 'Adelaide', 'Very Warm', 110, 5),
     ]
 
 
-def test_get_single_user(db_connection):
+def test_get_single_property(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
     repo = PropertyRepo(db_connection)
 
-    user = repo.find(3)
+    property = repo.find(3)
     
-    assert user == Property(3,'Lou', 'Paine', 'LPaine', 'Fortran90') 
+    assert property == Property(3,'The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3) 
     
 
-def test_create_user(db_connection):
+def test_create_property(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
     repo = PropertyRepo(db_connection)
 
-    repo.create(Property(None,'The Orange', '24 Cervantes St', 'Valencia', 'Very Warm', 90, 6))
+    repo.create(Property(6,'The Orange', '24 Cervantes St', 'Valencia', 'Very Warm', 90, 6))
 
     result = repo.all()
     assert result == [
-        Property('The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1),
-        Property('The Laurels', '56 Secret Bay', 'Reykjavik', 'Cold', 30, 2),
-        Property('The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3),
-        Property('The Bananas', '1730 Clark St', 'Chicago', 'Windy', 65, 4),
-        Property('The Pines', '435 Melbourne Ave', 'Adelaide', 'Very Warm', 110, 5),
-        Property('The Orange', '24 Cervantes St', 'Valencia', 'Very Warm', 90, 6)
+        Property(1,'The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1),
+        Property(2,'The Laurels', '56 Secret Bay', 'Reykjavik', 'Cold', 30, 2),
+        Property(3,'The Roses', '34 Sphinx Lane', 'Cairo', 'Warm', 60, 3),
+        Property(4,'The Bananas', '1730 Clark St', 'Chicago', 'Windy', 65, 4),
+        Property(5,'The Pines', '435 Melbourne Ave', 'Adelaide', 'Very Warm', 110, 5),
+        Property(6,'The Orange', '24 Cervantes St', 'Valencia', 'Very Warm', 90, 6)
     ]
     
 
-def test_delete_user(db_connection):
+def test_delete_property(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
     repo = PropertyRepo(db_connection)
     

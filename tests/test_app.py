e55245aaf6e7f9399912c6642714be2d5bef01b6
Khalid Ham <Khalidoham@gmail.com>
2024-11-06 11:08:23 +0000

    HTTP and HTML tests V2

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,20 +2,57 @@ from playwright.sync_api import Page, expect
 from lib.property import Property
 
 # HTTP Tests
+def test_http_get_users(web_client, db_connection):
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    response = web_client.get('/users')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'KHam, BShariffali, LPaine, ATobarra, JONeill' 
+
+def test_http_post_adds_a_user(web_client, db_connection):
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    post_response = web_client.post('/users', data={
+        'first_name': 'Alan',
+        'surname': 'Turing',
+        'username': 'Alan_turing04',
+        'user_password': 'EnigmaAlan!'
+    })
+    assert post_response.status_code == 200
+    response = web_client.get('/users')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'KHam, BShariffali, LPaine, ATobarra, JONeill, Alan_turing04'
+
+def test_http_get_single_user(web_client, db_connection):
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    response = web_client.get('/users/ATobarra')
+    response.status_code == 200
+    response.data.decode('utf-8') == "User(4, 'Alberto', 'Tobarra', 'ATobarra', None)"
+
 def test_http_get_existing_properties(web_client, db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
     response = web_client.get('/properties')
-    expected_text = (
-                "The Ferns, London: Haunted, "
-        "The Laurels, Reykjavik: Cold, "
-        "The Roses, Cairo: Warm, "
-        "The Bananas, Chicago: Windy, " 
-        "The Pines, Adelaide: Very Warm"
-    )
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == expected_text
-    
+    assert response.data.decode('utf-8') == "The Ferns, The Laurels, The Roses, The Bananas, The Pines"
 
+def test_http_get_existing_property_from_id(web_client, db_connection):
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    response = web_client.get('/properties/1')
+    assert response.status_code == 200 
+    assert response.data.decode('utf-8') == "Property(1,'The Ferns', '123 Wembley Downs', 'London', 'Haunted', 50, 1)"
+
+def test_http_post_property(web_client, db_connection):
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    post_response = web_client.post('/properties', data={
+        "property_name": "The Oaks",
+        "street_address": "143 Alan Lane",
+        "city": "Megalopolis",
+        "property_description": "Very large",
+        "price_per_night": 145,
+        "host_id": 2
+        })
+    response = web_client.get('/properties')
+    assert post_response.status_code == 200
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "The Ferns, The Laurels, The Roses, The Bananas, The Pines, The Oaks"
 
 
 # HTML Tests
@@ -25,7 +62,7 @@ def test_get_existing_properties(page, test_web_address, db_connection):
     page.goto(f"http://{test_web_address}/home")
     div_tags = page.locator("div")
     expected_properties = [
-        "The Ferns, London: Haunted"
+        "The Ferns, London: Haunted",
         "The Laurels, Reykjavik: Cold",
         "The Roses, Cairo: Warm",
         "The Bananas, Chicago: Windy", 
@@ -43,8 +80,6 @@ def test_log_in_recognises_existing_users(page, test_web_address, db_connection)
     page.fill("input[name=password]", "Python24")
     h1_tags = page.locator('h1')
     expect(h1_tags).to_have_text("Welcome, Khalid Ham!")
-
-
 
 def test_post_registers_new_user(page, test_web_address, db_connection):
     page.set_default_timeout(1000)

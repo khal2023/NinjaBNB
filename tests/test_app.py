5c4dafbf60aa966165e4dca42be9eb5a44cf8299
Khalid Ham <Khalidoham@gmail.com>
2024-11-06 09:07:15 +0000

    http and html tests v1

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,44 +1,64 @@
-# from playwright.sync_api import Page, expect
-# from lib.property import Property
+from playwright.sync_api import Page, expect
+from lib.property import Property
 
-# # Get properties from database
-# def test_get_existing_properties(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/makers_bnb_database.sql")
-#     page.goto(f"http://{test_web_address}/home")
-#     div_tags = page.locator("div")
-#     expected_properties = [
-#         "The Ferns, London: Haunted"
-#         "The Laurels, Reykjavik: Cold",
-#         "The Roses, Cairo: Warm",
-#         "The Bananas, Chicago: Windy", 
-#         "The Pines, Adelaide: Very Warm"
-#     ]
-#     for i, text in enumerate(expected_properties):
-#         expect(div_tags.nth(i)).to_have_text(text)
-
-# def test_log_in_recognises_existing_users(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/makers_bnb_database.sql")
-#     page.goto(f"http://{test_web_address}/home")
-#     page.click("text=Log in")
-#     page.fill("input[name=username]", "Kham")
-#     page.fill("input[name=password]", "Python24")
-#     h1_tags = page.locator('h1')
-#     expect(h1_tags).to_have_text("Welcome, Khalid Ham!")
+# HTTP Tests
+def test_http_get_existing_properties(web_client, db_connection):
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    response = web_client.get('/properties')
+    expected_text = (
+                "The Ferns, London: Haunted, "
+        "The Laurels, Reykjavik: Cold, "
+        "The Roses, Cairo: Warm, "
+        "The Bananas, Chicago: Windy, " 
+        "The Pines, Adelaide: Very Warm"
+    )
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == expected_text
+    
 
 
 
-# def test_post_registers_new_user(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/makers_bnb_database.sql")
-#     page.goto(f"http://{test_web_address}/home")
-#     page.click("text=Register")
-#     page.fill("input[name=first_name]", "Alan")
-#     page.fill("input[name=surname]", "Turing")
-#     page.fill("input[name=username]", "Alan_turing04")
-#     page.fill("input[name=password]", "EnigmaAlan!")
-#     h1_tags = page.locator('h1')
-#     expect(h1_tags).to_have_text("Welcome, Alan Turing!")
+# HTML Tests
+def test_get_existing_properties(page, test_web_address, db_connection):
+    page.set_default_timeout(1000)
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    page.goto(f"http://{test_web_address}/home")
+    div_tags = page.locator("div")
+    expected_properties = [
+        "The Ferns, London: Haunted"
+        "The Laurels, Reykjavik: Cold",
+        "The Roses, Cairo: Warm",
+        "The Bananas, Chicago: Windy", 
+        "The Pines, Adelaide: Very Warm"
+    ]
+    for i, text in enumerate(expected_properties):
+        expect(div_tags.nth(i)).to_have_text(text)
 
-# # Post property data to database
+def test_log_in_recognises_existing_users(page, test_web_address, db_connection):
+    page.set_default_timeout(1000)
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    page.goto(f"http://{test_web_address}/home")
+    page.click("text=Log in")
+    page.fill("input[name=username]", "Kham")
+    page.fill("input[name=password]", "Python24")
+    h1_tags = page.locator('h1')
+    expect(h1_tags).to_have_text("Welcome, Khalid Ham!")
+
+
+
+def test_post_registers_new_user(page, test_web_address, db_connection):
+    page.set_default_timeout(1000)
+    db_connection.seed("seeds/makers_bnb_database.sql")
+    page.goto(f"http://{test_web_address}/home")
+    page.click("text=Register")
+    page.fill("input[name=first_name]", "Alan")
+    page.fill("input[name=surname]", "Turing")
+    page.fill("input[name=username]", "Alan_turing04")
+    page.fill("input[name=password]", "EnigmaAlan!")
+    h1_tags = page.locator('h1')
+    expect(h1_tags).to_have_text("Welcome, Alan Turing!")
+
+# Post property data to database
 
 
 # """

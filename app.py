ab2ac2bf8fea69a1f5a10f157b589cc9cb354dc9
Khalid Ham <Khalidoham@gmail.com>
2024-11-07 16:06:39 +0000

    removed containers

--- app.py
+++ app.py
@@ -112,7 +112,6 @@ def http_get_existing_properties():
 if __name__ == '__main__':
     app.run(
         debug=True,
-        port=int(os.environ.get('PORT', 5001)),
-        host="0.0.0.0"
+        port=int(os.environ.get('PORT', 5001))
     )
     
